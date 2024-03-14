b60e8e6792cdabbacd4b618568f191e297b42114
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2024-03-14 14:10:22 +0200

    simplified entrypoint exec

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,11 +17,6 @@ whoami
 which python
 echo $PATH
 VERSION=$(jupyter lab --version)
-
-if [ -z "$@"]; then
-  echo "Starting Jupyter Lab"
-  jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --ContentsManager.allow_hidden=True --ServerApp.token='' --no-browser --debug --ServerApp.disable_check_xsrf=True --ResourceUseDisplay.mem_warning_threshold=0.2
-else
-  echo $@
-  exec "$@"
-fi
+echo "Starting Jupyter Lab"
+echo "Jupyter Lab version: $VERSION"
+jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --ContentsManager.allow_hidden=True --ServerApp.token='' --no-browser --debug --ServerApp.disable_check_xsrf=True --ResourceUseDisplay.mem_warning_threshold=0.2
