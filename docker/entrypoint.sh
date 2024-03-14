7251c51a5f0427246ecc2bea9a819fa296f11074
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2024-03-14 14:14:07 +0200

    simplify entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,4 +19,4 @@ echo $PATH
 VERSION=$(jupyter lab --version)
 echo "Starting Jupyter Lab"
 echo "Jupyter Lab version: $VERSION"
-jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --ContentsManager.allow_hidden=True --ServerApp.token='' --no-browser --debug --ServerApp.disable_check_xsrf=True --ResourceUseDisplay.mem_warning_threshold=0.2
+jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --ContentsManager.allow_hidden=True --ServerApp.token='' --no-browser --debug --ServerApp.disable_check_xsrf=True
